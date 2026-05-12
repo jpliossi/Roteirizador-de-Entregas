35bd4a3c76c589e28c4c6d69246077ef4242cf37
José Paulo <jpliossi@gmail.com>
2026-05-12 15:25:51 -0400

    feat: implementa efetivacao de rota com integração microserviços e correção de host rails

--- management-api/config/routes.rb
+++ management-api/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :enderecos
+  resources :enderecos do
+    collection do
+      put :atualizar_status
+    end
+  end
   resources :motoristas
   resources :veiculos
   get "up" => "rails/health#show", as: :rails_health_check
